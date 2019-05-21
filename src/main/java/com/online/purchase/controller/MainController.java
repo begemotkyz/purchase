326640cff83b07592c3f4fab4eefbbfea2dc0be1
Alina <alina.zhakypova@iaau.edu.kg>
2019-05-21 23:54:40 +0600

    Add MainController

---
+++ src/main/java/com/online/purchase/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.online.purchase.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController  {
+
+
+    @RequestMapping("/")
+    public String getHomePage(){
+        return "index";
+    }
+}
