80dda41d1f87c18e8d20427b1f13565d8283a696
Alina <alina.zhakypova@iaau.edu.kg>
2019-05-22 23:57:50 +0600

    ProductController UserController

---
+++ src/main/java/com/online/purchase/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.online.purchase.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+    @RequestMapping("/product/list")
+    public String getProductListPage(){
+        return "product-list";
+    }
+    @RequestMapping("/product/edit")
+    public String getProductEditPage(){
+        return "product-edit";
+    }
+}
